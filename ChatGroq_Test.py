4c7051b6da44c221ee5aee1dbfa687309d405da4
Ma Thandar <mamatda@gmail.com>
2025-01-29 20:42:45 +0630

    Update ChatGroq_Test.py

--- ChatGroq_Test.py
+++ ChatGroq_Test.py
@@ -27,4 +27,4 @@ loader = WebBaseLoader("https://jobs.nike.com/job/R-49848?from=job%20search%20fu
 #https://jobs.nike.com/job/R-33460
 page_data=loader.load().pop().page_content
 #print(page_data)
-#st.write(page_data)
+st.write(page_data)
