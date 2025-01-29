4bc0c96a403f3f3b20a7951da7ba611aa045246c
Ma Thandar <mamatda@gmail.com>
2025-01-29 16:03:08 +0630

    Update ChatGroq_Test.py

--- ChatGroq_Test.py
+++ ChatGroq_Test.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from datetime import date
 from langchain_groq import ChatGroq
+import chromadb
+
+chroma_client = chromadb.Client()
+collection = chroma_client.create_collection(name="my_collection")
 
 st.set_page_config(layout="wide")
 
