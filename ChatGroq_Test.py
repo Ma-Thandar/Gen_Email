1f869d66ed43452658bd3e1ed0bae7e8f295fa51
Ma Thandar <mamatda@gmail.com>
2025-01-29 16:07:26 +0630

    Update ChatGroq_Test.py

--- ChatGroq_Test.py
+++ ChatGroq_Test.py
@@ -4,7 +4,7 @@ from langchain_groq import ChatGroq
 import chromadb
 
 chroma_client = chromadb.Client()
-collection = chroma_client.create_collection(name="my_collection")
+collection = chroma_client.create_collection(name="new_collection")
 
 st.set_page_config(layout="wide")
 
